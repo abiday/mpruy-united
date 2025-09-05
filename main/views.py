c87c5beaf7e7336fca95e05c779e4bbfcd1c6594
abiday <abiddayyan51@gmail.com>
2025-09-05 14:38:53 +0700

    debug view

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'appname' : 'Mpruy United Shop',
+        'name': 'Abid Dayyan Putra Rahardjo',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
+
